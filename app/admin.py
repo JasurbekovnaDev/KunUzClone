35b99e54e67e3aa1ffd73e07756b09f25fa2da05
JasurbekovnaDev <nargizahusanbayeva22@gmail.com>
2026-02-11 17:54:01 +0500

    day-1

---
+++ app/admin.py
@@ -0,0 +1,28 @@
+from tkinter import Image
+from django.contrib import admin
+from app.models import News,Category
+
+# Register your models here.
+
+# 1 - способ
+
+# admin.site.register(Category)
+# admin.site.register(News)
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'publish_time', 'status', 'category']
+    list_filter = ['status', 'created_time', 'publish_time']
+    prepopulated_fields = {'slug':('title',)}
+    date_hierarchy = 'publish_time'
+    search_fields = ['']
+    ordering = ['title']
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    search_fields = ['name']
+
+
+
